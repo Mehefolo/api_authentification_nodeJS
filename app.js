925cc11fef7f66f29957cd969dd4da7b221e47cc
Mehefolo <koulibalymehefolo@gmail.com>
2021-02-18 19:42:14 +0100

    ajour

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var app = express();
 var db = require('./db');
+var cors = require('cors');
+
+
+app.use(cors());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+  });
+
 
 var UserController = require('./user/UserController');
 app.use('/user', UserController);
